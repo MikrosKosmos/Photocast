baedff79acfa999a0e3a6cdf93769302d2686c79
Pronoy Mukherjee <mukherjee_pronoy@yahoo.in>
2020-05-05 23:58:06 +0530

    Added Update Login Details. Modified the login master table.
    Signed-off-by: Pronoy Mukherjee <mukherjee_pronoy@yahoo.in>

--- Microservices/AuthenticationService/DB/Tables/sp_tbl_LoginMaster.sql
+++ Microservices/AuthenticationService/DB/Tables/sp_tbl_LoginMaster.sql
@@ -13,16 +13,28 @@ begin
             create table tbl_LoginMaster
             (
                 id           int primary key auto_increment,
-                email_id     varchar(255) DEFAULT null,
-                password     varchar(255) default null,
-                phone_number varchar(15)                NOT NULL,
-                role         varchar(30)                not null,
-                is_active    tinyint      default 1,
-                created_by   int                        not null,
-                created      timestamp    default now() not null,
-                modified_by  int          default null,
-                modified     timestamp    default null
+                email_id     varchar(255) UNIQUE DEFAULT null,
+                password     varchar(255)        default null,
+                phone_number varchar(15) UNIQUE                NOT NULL,
+                role         varchar(30)                       not null,
+                is_active    tinyint             default 1,
+                created_by   int                               not null,
+                created      timestamp           default now() not null,
+                modified_by  int                 default null,
+                modified     timestamp           default null
             );
+        end;
+    end if;
+    if not exists(
+            select 1
+            from information_schema.COLUMNS
+            where TABLE_SCHEMA = currentSchema
+              and TABLE_NAME = 'tbl_LoginMaster'
+              and COLUMN_NAME = 'user_id'
+        ) then
+        begin
+            alter table tbl_LoginMaster
+                add column user_id int NOT NULL after role;
         end;
     end if;
 end;
