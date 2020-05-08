22fe8759c252fa52fdfabf293433871c10e1c598
Pronoy Mukherjee <mukherjee_pronoy@yahoo.in>
2020-05-08 23:54:56 +0530

    Added Customer and vendor Tables and Updated the Login SP.
    Signed-off-by: Pronoy Mukherjee <mukherjee_pronoy@yahoo.in>

--- Microservices/UserService/DB/StoredProcedures/sp_ValidateLogin.sql
+++ Microservices/UserService/DB/StoredProcedures/sp_ValidateLogin.sql
@@ -2,19 +2,65 @@ drop procedure if exists sp_ValidateLogin;
 create procedure sp_ValidateLogin(parPhone varchar(15), parOTP int, parEmail varchar(255), parPassword varchar(255))
 begin
     set @isValid = 0;
+    set @userId = 0;
     if length(parPhone) > 0 and length(parOTP) > 0 then
         select fn_ValidateOTP(parPhone, parOTP) into @isValid;
+        select user_id from tbl_LoginMaster where phone_number = parPhone and is_active = 1 limit 1;
     else
-        select id
-        into @isValid
+        select user_id
+        into @userId
         from tbl_LoginMaster
         where email_id = parEmail
           and password = parPassword
           and is_active = 1;
+        if @userId > 0 then
+            set @isValid = 1;
+        end if;
     end if;
     if @isValid > 0 then
-        #TODO: get the user details.
-        select 1 as id;
+        set @table = '';
+        select role into @table from tbl_LoginMaster where user_id = @userId;
+        if @table = 'tbl_VendorMaster' then
+            select v.id,
+                   first_name,
+                   last_name,
+                   email_id,
+                   phone_number,
+                   gender,
+                   vendor_type,
+                   company_brand_name,
+                   address_1,
+                   address_2,
+                   city_id,
+                   pincode,
+                   gps_lat,
+                   gps_long,
+                   status_id,
+                   s.status_name,
+                   used_referral_code,
+                   referral_code
+            from tbl_VendorMaster v
+                     left join tbl_StatusMaster s
+                               on v.status_id = s.id
+            where v.id = @userId
+              and v.is_active = 1;
+        else
+            select c.id,
+                   first_name,
+                   last_name,
+                   email,
+                   phone_number,
+                   gender,
+                   used_referral_code,
+                   referral_code,
+                   status_id,
+                   s.status_name
+            from tbl_CustomerMaster c
+                     left join tbl_StatusMaster s
+                               on c.status_id = s.id
+            where c.id = @userId
+              and c.is_active = 1;
+        end if;
     else
         select -1 as id;
     end if;
---
+++ Microservices/UserService/DB/Tables/sp_tbl_VendorMaster.sql
@@ -0,0 +1,42 @@
+drop procedure if exists sp_tbl_VendorMaster;
+create procedure sp_tbl_VendorMaster()
+begin
+    declare currentSchema varchar(50) default '';
+    select database() into currentSchema;
+    if not exists(
+            select 1
+            from information_schema.TABLES
+            where TABLE_SCHEMA = currentSchema
+              and TABLE_NAME = 'tbl_VendorMaster'
+        ) then
+        begin
+            create table tbl_VendorMaster
+            (
+                id                 int primary key auto_increment           not null,
+                first_name         varchar(255)                             not null,
+                last_name          varchar(255)                             not null,
+                email_id           varchar(255),
+                phone_number       varchar(15)                              not null,
+                gender             enum ('M','F')                           not null,
+                vendor_type        ENUM ('PERSONAL','COMPANY')              not null,
+                company_brand_name varchar(255)   default null,
+                address_1          varchar(255)   default null,
+                address_2          varchar(255)   default null,
+                city_id            int            default null,
+                pincode            int            default null,
+                gps_lat            decimal(30, 8) default null,
+                gps_long           decimal(30, 8) default null,
+                status_id          int            default null,
+                used_referral_code varchar(100)   default null,
+                referral_code      varchar(100)   default null,
+                is_active          tinyint        default 1                 not null,
+                created_by         int                                      not null,
+                created            timestamp      default current_timestamp not null,
+                modified_by        int            default null,
+                modified           timestamp      default null
+            );
+        end;
+    end if;
+end;
+call sp_tbl_VendorMaster();
+drop procedure if exists sp_tbl_VendorMaster;
