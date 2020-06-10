bd5c2ff2c7218d2ed533fdaa2b5ba402c88f74bb
Pronoy Mukherjee <mukherjee_pronoy@yahoo.in>
2020-06-10 10:02:21 +0530

    Added Tables.
    Signed-off-by: Pronoy Mukherjee <mukherjee_pronoy@yahoo.in>

---
+++ Microservices/SocialMediaService/DB/Tables/sp_tbl_CommentMaster.sql
@@ -0,0 +1,30 @@
+drop procedure if exists sp_tbl_CommentMaster;
+create procedure sp_tbl_CommentMaster()
+begin
+    declare currentSchema varchar(50) default '';
+    select database() into currentSchema;
+    if not exists(
+            select 1
+            from information_schema.TABLES
+            where TABLE_SCHEMA = currentSchema
+              and TABLE_NAME = 'tbl_CommentMaster'
+        ) then
+        begin
+            create table tbl_CommentMaster
+            (
+                id          int primary key auto_increment      not null,
+                post_id     int                                 not null,
+                user_id     int                                 not null,
+                role        varchar(100)                        not null,
+                comment     text      default null,
+                is_active   tinyint   default 1                 not null,
+                created_by  int                                 not null,
+                created     timestamp default current_timestamp not null,
+                modified_by int       default null,
+                modified    timestamp default null
+            );
+        end;
+    end if;
+end;
+call sp_tbl_CommentMaster;
+drop procedure if exists sp_tbl_CommentMaster;
---
+++ Microservices/SocialMediaService/DB/Tables/sp_tbl_LikeMaster.sql
@@ -0,0 +1,29 @@
+drop procedure if exists sp_tbl_LikeMaster;
+create procedure sp_tbl_LikeMaster()
+begin
+    declare currentSchema varchar(50) default '';
+    select database() into currentSchema;
+    if not exists(
+            select 1
+            from information_schema.TABLES
+            where TABLE_SCHEMA = currentSchema
+              and TABLE_NAME = 'tbl_LikeMaster'
+        ) then
+        begin
+            create table tbl_LikeMaster
+            (
+                id          int primary key auto_increment      not null,
+                post_id     int                                 not null,
+                user_id     int                                 not null,
+                role        varchar(100)                        not null,
+                is_active   tinyint   default 1                 not null,
+                created_by  int                                 not null,
+                created     timestamp default current_timestamp not null,
+                modified_by int       default null,
+                modified    timestamp default null
+            );
+        end;
+    end if;
+end;
+call sp_tbl_LikeMaster();
+drop procedure if exists sp_tbl_LikeMaster;
