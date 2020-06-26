c8af4900384242a594c05672b2fe2e73af60ad0d
Pronoy Mukherjee <mukherjee_pronoy@yahoo.in>
2020-06-26 21:11:35 +0530

    Added API logger and token tables. Update the post creation SP.
    Signed-off-by: Pronoy Mukherjee <mukherjee_pronoy@yahoo.in>

--- Microservices/SocialMediaService/DB/StoredProcedures/sp_CreatePost.sql
+++ Microservices/SocialMediaService/DB/StoredProcedures/sp_CreatePost.sql
@@ -1,6 +1,6 @@
 drop procedure if exists sp_CreatePost;
-create procedure sp_CreatePost(parVendorId int, parPostDesc text, parImageUrl varchar(255), parFirstName varchar(255),
-                               parLastName varchar(255))
+create procedure sp_CreatePost(parVendorId int, parPostDesc text, parImageUrl varchar(255),
+                               parFirstName varchar(255), parLastName varchar(255))
 begin
     if length(parImageUrl) > 0 then
         insert into tbl_PostMaster(vendor_id, post_description, image_url, first_name, last_name,
