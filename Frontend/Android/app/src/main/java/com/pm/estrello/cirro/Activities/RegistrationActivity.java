98694e3b3d6c3d168c4f1b1a8de02ec03a6a17a8
Pronoy Mukherjee <mukherjee_pronoy@yahoo.in>
2020-07-07 23:51:37 +0530

    Added Navigation graph and fragment.

---
+++ Frontend/Android/app/src/main/java/com/pm/estrello/cirro/Activities/RegistrationActivity.java
@@ -0,0 +1,16 @@
+package com.pm.estrello.cirro.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.pm.estrello.cirro.R;
+
+public class RegistrationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+    }
+}
