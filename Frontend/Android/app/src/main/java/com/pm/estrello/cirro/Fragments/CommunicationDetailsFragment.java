b6c2b5262632dba6b52430469861757d9da10fd6
Pronoy Mukherjee <mukherjee_pronoy@yahoo.in>
2020-07-08 18:51:53 +0530

    Added CommunicationDetailsFragment.

--- Frontend/Android/app/src/main/java/com/pm/estrello/cirro/Fragments/CommunicationDetailsFragment.java
+++ Frontend/Android/app/src/main/java/com/pm/estrello/cirro/Fragments/CommunicationDetailsFragment.java
@@ -4,33 +4,33 @@ import android.os.Bundle;
 
 import androidx.appcompat.widget.AppCompatEditText;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.RadioGroup;
 
 import com.google.android.material.button.MaterialButton;
 import com.pm.estrello.cirro.R;
 
-public class BasicDetailsFragment extends Fragment {
-    private AppCompatEditText _firstName;
-    private AppCompatEditText _lastName;
-    private RadioGroup _radioGroup;
+public class CommunicationDetailsFragment extends Fragment {
+    private AppCompatEditText _phoneNumber;
+    private AppCompatEditText _emailId;
     private MaterialButton _nextButton;
     private MaterialButton _backButton;
 
-    public BasicDetailsFragment() {
+    public CommunicationDetailsFragment() {
         // Required empty public constructor
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_basic_details, container, false);
-        _firstName = view.findViewById(R.id.firstName);
-        _lastName = view.findViewById(R.id.lastName);
-        _radioGroup = view.findViewById(R.id.genderRadioGroup);
+        View view = inflater.inflate(R.layout.fragment_communication_details, container, false);
+        _emailId = view.findViewById(R.id.emailId);
+        _phoneNumber = view.findViewById(R.id.phoneNumber);
+        _nextButton = view.findViewById(R.id.communicationNextButton);
+        _backButton = view.findViewById(R.id.communicationBackButton);
         return view;
     }
 }
