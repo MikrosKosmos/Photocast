f8c2dea1f8fce4c6c42d0abec76b91b5c3b38f89
Pronoy Mukherjee <mukherjee_pronoy@yahoo.in>
2020-07-08 20:26:19 +0530

    Updated Basic Details.
    Signed-off-by: Pronoy Mukherjee <mukherjee_pronoy@yahoo.in>

--- Frontend/Android/app/src/main/java/com/pm/estrello/cirro/Fragments/BasicDetailsFragment.java
+++ Frontend/Android/app/src/main/java/com/pm/estrello/cirro/Fragments/BasicDetailsFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import androidx.appcompat.widget.AppCompatEditText;
 import androidx.fragment.app.Fragment;
 
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,6 +20,7 @@ public class BasicDetailsFragment extends Fragment {
     private RadioGroup _radioGroup;
     private MaterialButton _nextButton;
     private MaterialButton _backButton;
+    private char gender;
 
     public BasicDetailsFragment() {
         // Required empty public constructor
@@ -31,6 +33,36 @@ public class BasicDetailsFragment extends Fragment {
         _firstName = view.findViewById(R.id.firstName);
         _lastName = view.findViewById(R.id.lastName);
         _radioGroup = view.findViewById(R.id.genderRadioGroup);
+        _nextButton.setOnClickListener(v -> {
+            if (_radioGroup.getCheckedRadioButtonId() == -1) {
+
+            } else {
+                int radioId = _radioGroup.getCheckedRadioButtonId();
+                switch (radioId) {
+                    case R.id.radioButtonMale:
+                        gender = 'M';
+                        break;
+                    case R.id.radioButtonFemale:
+                        gender = 'F';
+                        break;
+                }
+            }
+        });
         return view;
+    }
+
+    /**
+     * Method to check whether the edit texts are not empty.
+     *
+     * @param editTexts: The Edit text to be checked.
+     * @return true if none are empty, else false.
+     */
+    private boolean isNotEmpty(AppCompatEditText[] editTexts) {
+        for (AppCompatEditText editText : editTexts) {
+            if (TextUtils.isEmpty(editText.getText())) {
+                return false;
+            }
+        }
+        return true;
     }
 }
